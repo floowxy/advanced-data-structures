c02249ea984233a96d50b17037a7d56dd1e91952
floowxy <alejsot1234@gmail.com>
2026-09-24 15:40:13 -0500

    kmp search

---
+++ PatternMatching/src/kmp_search.rs
@@ -0,0 +1,39 @@
+pub fn kmp_search(text : &str , pattern :&str) -> Vec<usize>{
+    if pattern.is_empty() {
+    return Vec::new();
+    }
+
+    let lps = build_lps(pattern);
+    
+    let text = text.as_bytes();
+    let pattern = pattern.as_bytes();
+
+    let len_text    = text.len();
+    let len_pattern = pattern.len();
+
+    let mut i: usize = 0;
+    let mut j: usize = 0;
+
+    let mut matches: Vec<usize> = Vec::new();
+
+    while i < len_text {
+        if text[i] == pattern[j]{
+            i +=1;
+            j +=1;
+        }
+        if j == len_pattern {
+            matches.push(i-j);
+            j = lps[j-1];
+        }    
+        else if i < len_text && text[i] != pattern[j]{
+            if j > 0 {
+                j = lps[j-1];
+            }
+            else {
+                i = i+1;
+            }
+        }
+    
+    }
+    matches
+}
