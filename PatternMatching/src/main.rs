db781cab28862c8fbfbf8f7481f16d5a830c875a
floowxy <alejsot1234@gmail.com>
2026-09-24 15:45:41 -0500

    main

--- PatternMatching/src/main.rs
+++ PatternMatching/src/main.rs
@@ -1,3 +1,17 @@
+mod build_lps;
+mod kmp_search;
+// modules 
+
+use kmp_search::kmp_search;
+// use module
+
 fn main() {
-    println!("Hello, world!");
+    let text = "ABABABAC";
+    let pattern = "ABA";
+
+    let matches = kmp_search(text, pattern);
+
+    println!("Text: {}", text);
+    println!("Pattern: {}", pattern);
+    println!("Matches: {:?}", matches);
 }
